0d126b12b99d0689f964f6a817845cc4e1347560
Aslan <aslan@redmintnetwork.fr>
2017-11-29 20:31:17 +0100

    Update rasKolDays.c

--- rasKolDays.c
+++ rasKolDays.c
@@ -112,9 +112,9 @@ void daysInMonth(char *argv1) {
                 previous.month = current.month;
                 previous.year = current.year;
                 daysInMonth = 1;
-                }
-                if(current.month == previous.month)
-                    daysInMonth++;
+            }
+            if(current.month == previous.month)
+                daysInMonth++;
         }
     }
 
